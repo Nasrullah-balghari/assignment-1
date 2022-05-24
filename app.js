3891b95becb8f985affabccf144d9564beb7deff
Nasrullah-balghari <nasrullahbalghari676@gmail.com>
2022-05-24 08:05:33 +0500

    assignment-1

---
+++ app.js
@@ -0,0 +1,29 @@
+//alert
+alert("Welcome to Javascript")
+alert("Error!Please enter a valid password")
+alert("Welcome to JS Land... \n Happy Coding!")
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+alert("Hello...I can run JS through my web browser's console")
+
+//variable for string
+var name = "Nasrullah balghari";
+alert(name);
+var message;
+message = "Hello World";
+alert(message);
+var name = "Nasrullah";
+var age = "23 years old";
+var course = "Certified Mobile application Development";
+alert(name);
+alert(age);
+alert(course);
+var order = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(order);
+var email = "My email address is nasrullahbalgari676@gmail.com";
+alert(email);
+var book = "I am trying to learn from the Book A smarter \n way to learn javaScript";
+alert(book);
+document.write("Yah!I can write HTML content through javaScript")
+var string ="--------&$*&%$&--------";
+alert(string);
